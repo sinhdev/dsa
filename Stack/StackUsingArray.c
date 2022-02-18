2c666af7ecd7fd69bc5c15695add11a6d2eb79b5
sinhdev <sinhnx.dev@gmail.com>
2022-02-18 16:10:10 +0700

    update stack demo

--- Stack/StackUsingArray.c
+++ Stack/StackUsingArray.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 
-struct stack{
+typedef struct {
     int data[50];
-    int maxIndex;
-    int topIndex;
-};
+    int top;
+} stack;
 
-int push(int value, struct stack *s);
-int pop(int *topValue, struct stack *s);
+int push(int value, stack *s);
+int pop(int *topValue, stack *s);
+int isEmpty(stack *s);
+int isFull(stack *s);
 
 int main(int count, char **args){
     //init myStack
-    struct stack myStack;
-    myStack.maxIndex = 49;
-    myStack.topIndex = -1;
+    stack myStack;
+    myStack.top = -1;
 
     push(1, &myStack);
     push(2, &myStack);
     push(3, &myStack);
     push(4, &myStack);
-    push(5, &myStack);
 
     int top;
     while(pop(&top, &myStack)){
@@ -27,29 +26,44 @@ int main(int count, char **args){
     }
     return 0;
 }
-int pop(int *topValue, struct stack *s){
+int pop(int *topValue, stack *s){
     //Step 1:
-    if(s->topIndex == -1){
+    if(isEmpty(s)){
         //Step 2:
         return 0;
     }
     //Step 3:
-    *topValue = s->data[s->topIndex];
+    *topValue = s->data[s->top];
     //Step 4:
-    s->topIndex -= 1;
+    s->top -= 1;
     //Step 5:
     return 1;
 }
-int push(int value, struct stack *s){
+int push(int value, stack *s){
     //Step 1:
-    if(s->maxIndex == s->topIndex){
+    if(isFull(s)){
         //Step 2:
         return 0;
     }
     //Step 3:
-    s->topIndex += 1;
+    s->top += 1;
     //Step 4:
-    s->data[s->topIndex] = value;
+    s->data[s->top] = value;
     //Step 5:
     return 1;
+}
+
+int isEmpty(stack *s){
+    if (s->top == -1) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+int isFull(stack *s) {
+    if (s->top == 49) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
